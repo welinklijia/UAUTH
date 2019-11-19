9a0ae401bd9c92154c64d4d0aad8a10d468d4e9f
lijia <lijia2015@boco.com.cn>
2019-11-19 14:09:35 +0800

    删除工程

--- unitive-rule-manage/unitive-rule-manage-dao/src/main/java/com/boco/unitive/rule/dao/TfaGaiaRuleSubAlarmMapper.java
+++
@@ -1,7 +0,0 @@
-package com.boco.unitive.rule.dao;
-
-import com.boco.unitive.rule.core.GeneralMapper;
-import com.boco.unitive.rule.entity.TfaGaiaRuleSubAlarm;
-
-public interface TfaGaiaRuleSubAlarmMapper extends GeneralMapper<TfaGaiaRuleSubAlarm> {
-}
